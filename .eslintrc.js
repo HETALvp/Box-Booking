9cfb8f470ba097849e79874236509f94559bfb76
HETALvp <hetuparmar98@gmail.com>
2024-05-15 10:28:29 +0530

    prettier and eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['unused-imports'],
+  rules: {
+    'prettier/prettier': ['warn'],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-console': 'error',
+    'react-native/no-unused-styles': 'warn',
+    'react/no-unused-prop-types': 'warn',
+    'unused-imports/no-unused-imports': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
